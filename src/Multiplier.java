a2d42a6151c0f964a94eff07bbeccbcc3c3836be
lunaro <136283802+Lunaro-Ai@users.noreply.github.com>
2026-02-20 23:11:13 -0500

    Updated Multiplier

--- src/Multiplier.java
+++ src/Multiplier.java
@@ -1,13 +1,20 @@
 public class Multiplier {
     public static void multiply(Word32 a, Word32 b, Word32 result) {
 
-
-        Bit currA,currB;
-        currA = new Bit(false);
-        currB = new Bit(false);
-
-
-
-
+        Bit currB = new Bit(false);
+        int bitPosition = 0;
+        Word32 temp = new Word32();
+        Word32 sum = new Word32();
+        Word32 zero = new Word32();
+        for (int i = 31; i >= 0; i--) {
+            b.getBitN(i,currB);
+            if (currB.getValue()) {
+                Shifter.LeftShift(a,bitPosition,temp);
+                Adder.add(temp,sum,sum);
+                zero.copy(temp);
+            }
+            bitPosition++;
+        }
+        sum.copy(result);
     }
 }
--- src/Shifter.java
+++ src/Shifter.java
@@ -1,15 +1,13 @@
 public class Shifter {
+
+
     public static void LeftShift(Word32 source, int amount, Word32 result) {
         for (int i = 31; i >= 0; i--) {
             int rA = amount % 32;
-            int index = i - rA;
+            int index = i-rA;
             Bit current = new Bit(false);
             source.getBitN(i, current);
-            if (index >= 0) {
-                result.setBitN(index, current);
-
-            }
-
+            if (index >= 0) result.setBitN(index, current);
         }
     }
     public static void RightShift(Word32 source, int amount, Word32 result) {
