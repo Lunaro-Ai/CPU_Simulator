246a6546464c6c1a7291d92794226cc73fef8997
lunaro <136283802+Lunaro-Ai@users.noreply.github.com>
2026-03-09 21:18:11 -0400

    Updated mem class and ALU class

--- src/ALU.java
+++ src/ALU.java
@@ -6,8 +6,72 @@ public class ALU {
     public Bit less = new Bit(false);
     public Bit equal = new Bit(false);
 
-    public void doInstruction(){
-
+    public void doInstruction() {
+        int code = getOp();
+        if (code == 1) Adder.add(op1, op2, result);
+        else if (code == 2) op1.and(op2, result);
+        else if (code == 3) Multiplier.multiply(op1, op2, result);
+        else if (code == 4) {
+            int shift = fiveLowestBits(op2);
+            Shifter.LeftShift(op1, shift, result);
+        }
+        else if (code == 5) Adder.subtract(op1,op2,result);
+        else if (code == 6) op1.or(op2,result);
+        else if (code == 7) {
+            int shift = fiveLowestBits(op2);
+            Shifter.RightShift(op1,shift,result);
+        }
+        else if (code == 11) {
+            Adder.subtract(op1,op2,result);
+            int res = wordToInt(result);
+            less.assign(false);
+            equal.assign(false);
+            if (res == 0) {
+                equal.assign(true);
+            } else if (res<0) {
+                less.assign(true);
+            } else {
+                less.assign(false);
+            }
+        }
 
     }
+
+
+    public int getOp() {
+        int val = 1;
+        int rval = 0;
+        Bit c = new Bit(true);
+        for (int i = 4; i >= 0; i--) {
+            instruction.getBitN(i, c);
+            if (c.getValue()) rval += val;
+            val *= 2;
+        }
+        return rval;
+    }
+
+    public int wordToInt(Word32 word) {
+        int val = 1;
+        int rval = 0;
+        Bit c = new Bit(true);
+        for (int i = 31; i >= 0; i--) {
+            word.getBitN(i, c);
+            if (c.getValue()) rval += val;
+            val *= 2;
+        }
+        return rval;
+    }
+
+    public int fiveLowestBits(Word32 word) {
+        int postion = 1;
+        int shiftAmount = 0;
+        Bit c = new Bit(true);
+        for (int i = 31; i >= 27; i--) {
+            word.getBitN(i, c);
+            if (c.getValue()) shiftAmount += postion;
+            postion *= 2;
+        }
+        return shiftAmount;
+    }
 }
+
--- src/ALUTest.java
+++ src/ALUTest.java
@@ -27,29 +27,38 @@ class ALUTest {
         alu.doInstruction();
         assertEquals(300,TestConverter.toInt(alu.result));
 
+
         setupTest(alu,2,64,96); // AND
         alu.doInstruction();
         assertEquals(64,TestConverter.toInt(alu.result));
+
+
 
         setupTest(alu,3,6,6); // MULTIPLY
         alu.doInstruction();
         assertEquals(36,TestConverter.toInt(alu.result));
 
+
+
         setupTest(alu,4,10,2); // Left Shift
         alu.doInstruction();
         assertEquals(40,TestConverter.toInt(alu.result));
+
 
         setupTest(alu,5,100,200); // Subtract
         alu.doInstruction();
         assertEquals(-100,TestConverter.toInt(alu.result));
 
+
         setupTest(alu,6,64,96); // Or
         alu.doInstruction();
         assertEquals(96,TestConverter.toInt(alu.result));
 
+
         setupTest(alu,7,1000,2); // Right Shift
         alu.doInstruction();
         assertEquals(250,TestConverter.toInt(alu.result));
+
 
         setupTest(alu,11,1000,2); // Compare - less
         alu.doInstruction();
@@ -65,5 +74,14 @@ class ALUTest {
         alu.doInstruction();
         assertFalse(alu.less.getValue());
         assertTrue(alu.equal.getValue());
+
+
+
+
+
+
+
+
+
     }
 }
